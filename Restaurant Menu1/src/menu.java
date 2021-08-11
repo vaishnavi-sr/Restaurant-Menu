8cdb7f3a88302b6186729c99c7c3fe827eebec8c
vaishnavi <vaishu.srini15@gmail.com>
2021-08-10 20:38:13 -0500

    Modified files for Studio part2

--- Restaurant Menu1/src/menu.java
+++ Restaurant Menu1/src/menu.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class menu {
+public class menu {
+    private  String foods;
+
+    public static void main(String[] args) {
+        menuItems s = new menuItems();
+        s.setPrice(20);
+        s.setUpdatedDate(true);
+        s.setCategory();
+
+    }
 }
--- Restaurant Menu1/src/menuItems.java
+++ Restaurant Menu1/src/menuItems.java
@@ -2,21 +2,43 @@ public class menuItems {
     private int price;
     private String description;
     private String category; /*Appetisers, Main course, Dessert */
+    private boolean updatedDate;
+    private String appetisers;
+    private String MainCourse;
+    private String dessert;
 
-    public boolean isUpdatedDate() {
+
+    public boolean getUpdatedDate() {
         return updatedDate;
     }
 
     public void setUpdatedDate(boolean updatedDate) {
         this.updatedDate = updatedDate;
+        if (updatedDate = true) {
+            System.out.println("The menu is updated recently and exciting new items are added.");
+        }else {
+            System.out.println("This is the old menu");
+
+        }
     }
 
-    private boolean updatedDate;
-
-    public menuItems(int price) {
-        this.price = price;
+    public void setPrice(int price) {
+        this.price = price + 10;
+        System.out.println("The bill with tax $10 added :" + this.price);
     }
-    public void setPrice(price){
-
+    public int getPrice(){
+        return price;
     }
+
+    public void setCategory(){
+        this.category = category;
+        System.out.println("The category has Appetizers, Main course and Desserts.");
+    }
+   public String getCategory(){
+        return category;
+   }
+   public void setDescription(){
+        this.description = description;
+       System.out.println(this.appetisers + "- Is the ");
+   }
 }
